c6662b6c754d5fe16cfcb3e28d9b33fdf29874f8
RiyanNaffa <riyannaffanusafara@gmail.com>
2025-09-13 20:52:22 +0700

    feat: add domain entity classes

---
+++ PortLog.Domain/Entities/Insight.cs
@@ -0,0 +1,28 @@
+namespace PortLog.Domain
+{
+    public class Insight
+    {
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public int TotalTrips { get; private set; }
+        public TimeSpan TotalHours { get; private set; }
+        public float TotalDistance { get; private set; }
+        public float AverageSpeed { get; private set; }
+        public float AverageFuelConsumption { get; private set; }
+
+        /// <summary>
+        /// Constructor <c>Insight</c> for immediate instantiation.
+        /// </summary>
+        public Insight(DateTime startDate, DateTime endDate, int totalTrips, TimeSpan totalHours, float totalDistance, float averageSpeed, float averageFuelConsumption)
+        {
+            if (endDate < startDate) throw new ArgumentException("End date cannot be earlier than start date");
+            StartDate = startDate;
+            EndDate = endDate;
+            TotalTrips = totalTrips;
+            TotalHours = totalHours;
+            TotalDistance = totalDistance;
+            AverageSpeed = averageSpeed;
+            AverageFuelConsumption = averageFuelConsumption;
+        }
+    }
+}
